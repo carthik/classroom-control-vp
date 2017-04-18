613bde47a65333a576d992f09c67b962ebb434ea
SeanTecha <sean.t@puppet.com>
2017-04-18 11:57:03 -0700

    Create files.pp

---
+++ site/review/manifests/files.pp
@@ -0,0 +1,17 @@
+class review::files {
+  file { '/etc/shells':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/review/shells',
+  }
+  
+  file { '/etc/motd':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => epp('review/motd.epp'),
+  }
+}
