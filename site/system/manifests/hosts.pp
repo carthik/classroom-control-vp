14ec34c0d985c4b587cde3b13d86d0962428dd48
N. Phillip Cole <nonplayer@users.noreply.github.com>
2017-04-18 15:22:28 -0700

    resources added

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -3,28 +3,41 @@ class system::hosts {
     purge => true,
   }
 
-  host { 'master.puppetlabs.vm':
-    ensure => present,
-    host_aliases => ['master','puppet'],
-    ip => '171.17.0.1',
-  }
-
-  host { 'localhost':
-    ensure => present,
-    host_aliases => [
-      'localhost.localdomain',
-      'localhost4',
-      'localhost4.localdomain4',
-      'training.puppetlabs.vm',
-      'training'
-    ],
-    ip => '127.0.0.1',
-  }
-
-## Use your own IP, or the ::ipaddress fact
-  host { 'nonplayer.puppetlabs.vm':
-    ensure => present,
-    host_aliases => ['nonplayer'],
-    ip => $::ipaddress,
-  }
+host { 'ip6-allnodes':
+  ensure => 'present',
+  ip     => 'ff02::1',
+  target => '/etc/hosts',
+}
+host { 'ip6-allrouters':
+  ensure => 'present',
+  ip     => 'ff02::2',
+  target => '/etc/hosts',
+}
+host { 'ip6-localnet':
+  ensure => 'present',
+  ip     => 'fe00::0',
+  target => '/etc/hosts',
+}
+host { 'ip6-mcastprefix':
+  ensure => 'present',
+  ip     => 'ff00::0',
+  target => '/etc/hosts',
+}
+host { 'localhost':
+  ensure => 'present',
+  ip     => '127.0.0.1',
+  target => '/etc/hosts',
+}
+host { 'master.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['puppet'],
+  ip           => '172.17.0.1',
+  target       => '/etc/hosts',
+}
+host { 'nonplayer.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['nonplayer'],
+  ip           => '172.17.0.10',
+  target       => '/etc/hosts',
+}
 }
