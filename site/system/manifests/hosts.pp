8c157f653b4272226d69c14a5b43563df220d38b
Aaron Miller <gitamiller@users.noreply.github.com>
2017-04-18 18:22:28 -0400

    5.1 purge

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,6 +1,7 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+resources {'host':
+  purge => true,
+}
 host { 'gitamiller.puppetlabs.vm':
   ensure       => 'present',
   host_aliases => ['gitamiller'],
