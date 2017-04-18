a8cba2693396d3107d962029890e3f63c643aab7
N. Phillip Cole <nonplayer@users.noreply.github.com>
2017-04-18 15:19:41 -0700

    updated hposts exercise 5.1

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,30 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+  resources {'host':
+    purge => true,
+  }
+
+  host { 'master.puppetlabs.vm':
+    ensure => present,
+    host_aliases => ['master','puppet'],
+    ip => '171.17.0.1',
+  }
+
+  host { 'localhost':
+    ensure => present,
+    host_aliases => [
+      'localhost.localdomain',
+      'localhost4',
+      'localhost4.localdomain4',
+      'training.puppetlabs.vm',
+      'training'
+    ],
+    ip => '127.0.0.1',
+  }
+
+## Use your own IP, or the ::ipaddress fact
+  host { 'nonplayer.puppetlabs.vm':
+    ensure => present,
+    host_aliases => ['nonplayer'],
+    ip => $::ipaddress,
+  }
 }
