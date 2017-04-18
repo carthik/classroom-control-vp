8c921f4667246c0e26d9e5f845064f0dfaa6ef3a
SeanTecha <sean.t@puppet.com>
2017-04-18 15:57:16 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,11 +43,8 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  system::managed_user { 'sean':
-    ensure => present,
-  }
-  
-  system::managed_user { 'sean2':
-    ensure => present,
+  $password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
+  system::managed_user { 'sean', 'sean2':
+    password => $password,
   }
 }
