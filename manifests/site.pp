d0009a28057eb1849a7957e2330e7fc484cb90d6
joelschoenberg <joel.schoenberg@hotmail.com>
2017-04-18 16:03:02 -0700

    Added doug

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,5 +42,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  system::managed_user { 'doug': 
+    password => $password,
+  }
+  
   include role::classroom
 }
