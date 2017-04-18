8ed44004b38d7bc3d852140f503468209f6b93ca
Arafat M <github@arafatm.com>
2017-04-18 12:10:13 -0700

    file mode is a string not fixnum

--- site/review/manifests/files.pp
+++ site/review/manifests/files.pp
@@ -1,9 +1,10 @@
 class review::files {
+
   file { '/etc/shells':
     ensure => file,
     owner  => root,
     group  => root,
-    mode   => 0644,
+    mode   => '0644',
     source => 'puppet:///modules/review/shells',
   }
 
@@ -11,7 +12,7 @@ class review::files {
     ensure => file,
     owner  => root,
     group  => root,
-    mode   => 0644,
+    mode   => '0644',
     source => epp('review/motd.epp'),
   }
 }
