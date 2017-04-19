292dce4f14984c2a64c7d8aa7be285a5dc6dde8d
Greg Larkin <greg.larkin@puppet.com>
2017-04-19 13:56:06 -0700

    Update default_realm.rb

--- site/kerberos/lib/facter/default_realm.rb
+++ site/kerberos/lib/facter/default_realm.rb
@@ -1,5 +1,7 @@
 # Write a fact that returns the value of running the command
 # "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
 Facter.add(:default_realm) do
-  "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
+  setcode do
+    /bin/awk '/default_realm/{print $NF}' /etc/krb5.conf
+  end
 end
