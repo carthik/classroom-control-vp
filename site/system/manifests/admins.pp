9548c5ae9e2975e7c671a9fcb135fc9e22146b2c
Greg Larkin <greg.larkin@puppet.com>
2017-04-19 10:19:03 -0700

    Update admins.pp

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,26 +1,35 @@
 class system::admins {
   require mysql::server
-  mysql_user { 'zack@localhost':
-    ensure => present,
-    max_queries_per_hour => 1200,
+  
+  $active_admins = [ 
+    'zack' => { max_queries_per_hour => 1200 },
+    'monica' => { max_queries_per_hour => 600 },
+    'brad' => { max_queries_per_hour => 600 },
+    'luke' => { max_queries_per_hour => 600 }
+  ]
+  
+  $inactive_admins = [ 'ralph' ]
+  
+  $active_admins.each |String $admin, Hash $data| {
+    $max_queries_per_hour = $data['max_queries_per_hour']
+  
+    mysql_user { "${admin}@localhost":
+      ensure => present,
+      max_queries_per_hour => $max_queries_per_hour,
+    }
+    
+    user { $admin:
+      ensure => present,
+    }
   }
-  mysql_user { 'monica@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'ralph@localhost':
-    ensure => absent,
-  }
-  mysql_user { 'brad@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-  mysql_user { 'luke@localhost':
-    ensure => present,
-    max_queries_per_hour => 600,
-  }
-
-  user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
-    ensure => present,
+  
+  $inactive_admins.each |String $admin| {
+    mysql_user { "${admin}@localhost":
+      ensure => absent,
+    }
+    
+    user { $admin:
+      ensure => absent,
+    }
   }
 }
