17a2ca4b0f6e8dfe9306fffaabbddcdd9f9a7ed7
Eric Putnam <putnam.eric@gmail.com>
2017-04-19 13:24:36 -0700

    Update admins.pp
    formatting stuff

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -2,25 +2,27 @@ class system::admins {
   require mysql::server
   $retired = [ 'ralph' ] 
   $admins = {
-    'brad' => { max_queries_per_hour => '600' }, 
+    'brad'   => { max_queries_per_hour => '600' }, 
     'monica' => { max_queries_per_hour => '600' }, 
-    'luke' => { max_queries_per_hour => '600' }, 
-    'zack' => { max_queries_per_hour => '1200' },
+    'luke'   => { max_queries_per_hour => '600' }, 
+    'zack'   => { max_queries_per_hour => '1200' },
   }
   $admins.each |$user, $params| { 
     mysql_user { "${user}@localhost":
-      ensure => present,
+      ensure               => present,
       max_queries_per_hour => $params['max_queries_per_hour'], 
     }
     user { $user:
-       ensure => present, managehome => true,
+       ensure     => present, 
+       managehome => true,
     } 
   }
   $retired.each |$user| {
     mysql_user { "${user}@localhost":
       ensure => absent, 
     }
-    user { $user: ensure => absent,
+    user { $user: 
+      ensure => absent,
     }
    }
  }
