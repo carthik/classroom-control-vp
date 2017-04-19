9b899e0227f26057271739e81f0dfe26676ce899
Tommy Speigner <tspeigner@gmail.com>
2017-04-19 11:42:21 -0700

    Update admins.pp

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,33 +1,18 @@
 class system::admins {
   ## Require the mysql server
   require mysql::server
-  
-  $admins = {
-    'monica' => {max_queries_per_hour => '600'},
-    'zack'   => {max_queries_per_hour => '1200'},
-    'luke'   => {max_queries_per_hour => '600'},
-    'tommy'  => {max_queries_per_hour => '900'},
-  }
-  $retired = ['ralph']
-  }
-  
-  $admins.each |$user,$params| {
-    mysql_user { "${user}@localhost":
-    ensure                => present,
-    max_queries_per_hour  => $params['max_queries_per_hour'],
-  }
-  
-  user { $user:
-    ensure     => present,
-    managehome => true,
-  }
-  
-  $retired.each |$user| {
-    mysql_user { "${user}@localhost":
-    ensure => absent,
-  }
-    
-  user { $user: ensure => absent,
-  } 
- }
+$retired = [ 'ralph' ] $admins = {
+'brad' => { max_queries_per_hour => '600' }, 'monica' => { max_queries_per_hour => '600' }, 'luke' => { max_queries_per_hour => '600' }, 'zack' => { max_queries_per_hour => '1200' },
+}
+$admins.each |$user, $params| { mysql_user { "${user}@localhost":
+ensure => present,
+max_queries_per_hour => $params['max_queries_per_hour'], }
+user { $user:
+ensure => present, managehome => true,
+} }
+$retired.each |$user| {
+mysql_user { "${user}@localhost":
+ensure => absent, }
+user { $user: ensure => absent,
+} }
 }
