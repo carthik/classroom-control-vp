809af7f51e5d607996eebccfaccee085535432e4
SeanTecha <sean.t@puppet.com>
2017-04-19 13:59:14 -0700

    Update default_realm.rb

--- site/kerberos/lib/facter/default_realm.rb
+++ site/kerberos/lib/facter/default_realm.rb
@@ -2,5 +2,5 @@
 # "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
 
 Facter.add('default_realm') do 
-  setcode 'awk '/default_realm/{print $NF}' /etc/krb5.conf'
+  setcode "awk '/default_realm/{print $NF}' /etc/krb5.conf"
 end
