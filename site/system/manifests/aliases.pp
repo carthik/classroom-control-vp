719bbb64d74f183ea85308f05b3b2bf5e1d7b84f
Hunter Haugen <hunter@puppet.com>
2017-04-19 09:29:51 -0700

    Update aliases with string data type

--- site/system/manifests/aliases.pp
+++ site/system/manifests/aliases.pp
@@ -1,16 +1,19 @@
+# System aliases
 class system::aliases (
-    $admin = 'root',
+  String  $admin = 'root',
 ) {
-    # uses $admin to build the aliases file
-    file { '/etc/aliases':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => epp('system/aliases.epp', { admin => $admin }),
-    }
-    exec { '/usr/bin/newaliases':
-        refreshonly => true,
-        subscribe   => File['/etc/aliases'],
-    }
+  # uses $admin to build the aliases file
+  file { '/etc/aliases':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => epp('system/aliases.epp', {
+      admin => $admin,
+    }),
+  }
+  exec { '/usr/bin/newaliases':
+    refreshonly => true,
+    subscribe   => File['/etc/aliases'],
+  }
 }
