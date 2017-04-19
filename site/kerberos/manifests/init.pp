cc7498bcbc1988661c04d167a66287313a4d6173
Arafat M <github@arafatm.com>
2017-04-19 15:15:49 -0700

    tyop

--- site/kerberos/manifests/init.pp
+++ site/kerberos/manifests/init.pp
@@ -2,7 +2,7 @@ class kerberos {
   # use an augeas resource to manage the default_realm
   # determine the proper context with the augtool shell
 
-  augeus { 'krb5.conf':
+  augeas { 'krb5.conf':
     context => '/files/etc/krb5.conf/libdefaults',
     changes => 'set default_realm PUPPETLABS.VM',
   }
