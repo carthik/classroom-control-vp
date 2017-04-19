30ed7afb86743246595edbb1ff2eba71ec2b59f4
Greg Larkin <greg.larkin@puppet.com>
2017-04-19 14:49:51 -0700

    Update init.pp

--- site/files/manifests/init.pp
+++ site/files/manifests/init.pp
@@ -13,7 +13,17 @@ class files {
     line   => 'root',
   }
   # Add a rule to cron.deny to deny jobs by default
-
+  file_line { 'disallow other cron jobs':
+    ensure => present,
+    path   => '/etc/cron.deny',
+    line   => '*',
+  }
+  
+  concat { '/etc/motd':
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
 
   # What concat resource is needed for this fragment to work?
   concat::fragment { 'motd header':
@@ -23,6 +33,16 @@ class files {
   }
 
   # Add a few fragments to be appended to /etc/motd
+  concat::fragment { 'motd 01':
+    target => '/etc/motd',
+    order  => '03',
+    content => "Hi there....\n",
+  }
 
+  concat::fragment { 'motd 02':
+    target => '/etc/motd',
+    order  => '04',
+    content => "Bye there....\n",
+  }
 
 }
