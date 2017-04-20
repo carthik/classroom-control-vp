a1e82be96c24013aadcc4911ca21b185f7891073
Aaron Miller <gitamiller@users.noreply.github.com>
2017-04-20 13:17:21 -0400

    11.1 site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,4 +45,5 @@ node default {
   include role::classroom
   $password = '$1$HdDw//gC$2VBiQ1x5blLPwNS.G.Iw21'
   system::managed_user { ['aaron', 'kaitlin', 'jose']: password => $password, }
+  notice(hiera("message"))
 }
